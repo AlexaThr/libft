fc3a2231eef8424031c0998e05389058ea010a7a
Alexandra Thorne <athorne@e1z3r3p12.42.us.org>
2019-06-18 13:30:37 -0700

    Memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,10 +6,19 @@
 /*   By: athorne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 17:47:39 by athorne           #+#    #+#             */
-/*   Updated: 2019/04/29 17:51:49 by athorne          ###   ########.fr       */
+/*   Updated: 2019/06/18 13:30:05 by athorne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
 void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *dest;
+	if (len == 0)
+		return (b);
+	dest = b;
+	while (len--)
+		*dest++ = (unsigned char)c;
+	return (b);
+}
